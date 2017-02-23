cd46da28328d4d3a60f31cd02ca765419c817b39
Cedric Ith <cedric.ith1@gmail.com>
2017-02-23 15:00:27 -0800

    Completed exercise 1

---
+++ exercise-2/publish.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(name='cedricith',
+                          token='C059F814B74352926ECECEEF1D854793',
+                          secret='s1vi1QSSJhnt2zly9/7kujf4NoNaC8dRMZzlknik')
+
+library('rsconnect')
+rsconnect::deployApp('/Users/Cedric/Desktop/INFO 210/module15-shiny/exercise-2')
